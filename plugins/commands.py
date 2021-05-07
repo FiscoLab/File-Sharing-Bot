059b6dbd65899b8c8abb5469e921d1cdefe75d80
FiscoLab <83391319+FiscoLab@users.noreply.github.com>
2021-05-07 15:14:16 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -88,7 +88,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
+            text = f"<b>○ Creator</b> : <a href='https://t.me/diago_x'>DIAGO</a>\n\n○ <b>Language</b> : <code>Python3</code>\n\n○ <b>Library</b> : Pyrogram \n\n○ <b>Server</b> : Heroku \n\n○ Source Code : 🔐\n\n© NexonHex",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
